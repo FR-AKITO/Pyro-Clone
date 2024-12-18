b1a8e7efab87f558fa4cdee2a665fee61e08ca2a
FR-AKITO <varshakoli638@gmail.com>
2024-12-18 14:49:48 +0530

    Update __init__.py

--- Clone/__init__.py
+++ Clone/__init__.py
@@ -19,7 +19,7 @@ StartTime = time.time()
 # Set up bot configurations
 API_ID = 25629197
 API_HASH = "fd41f8bacda97ab0a3ad120b30339978"
-TOKEN = "6528046179:AAHkcHbalYrhZjD4rjDXVagtYY1hiSK34zE"
+TOKEN = "7402282033:AAEv5Aq37Kf6vCj8k1GVnCEcnuDCIXWvHMk"
 
 #PYROGRAM BOT CLIENT
 bot = Client(name="CloneBot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Clone.Examples"))
